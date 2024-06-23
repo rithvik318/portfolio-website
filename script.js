711910790c26df545aeab6265a736e3f08668424
rithvik318 <153345925+rithvik318@users.noreply.github.com>
2024-06-24 00:48:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+    alert('Name: ' + name + '\nEmail: ' + email + '\nMessage: ' + message);
+    this.reset();
+});
